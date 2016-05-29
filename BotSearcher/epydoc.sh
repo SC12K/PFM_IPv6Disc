b0116cb3c0c79b0e979b9ffafbba4a0350e33700
datiner <fpm.nando@gmail.com>
2016-05-29 20:00:57 +0200

    Cleaning repo

--- BotSearcher/epydoc.sh
+++
@@ -1,5 +0,0 @@
-epydoc --html -o ./docs -v ./Interfaz/Interprete.py ./Core/GestorDeModulos.py ./Core/GestorDeResultados.py ./Core/GestorDeSondas.py ./Core/EjecutorSondeo.py ./Core/Modulos/Ejecutores/EjecutorSYN.py \
-./Core/DispensadorIPv6.py ./Core/Modulos/Dispensadores/D_IPv4MappedIPv6.py ./Core/Modulos/Dispensadores/D_IPv4TEST.py ./Core/Sonda.py ./Interfaz/Instruccion.py ./Interfaz/RepertorioGdS.py \
-./Core/GestorDeEjecucion.py ./Core/Modulos/Ejecutores/Ping.py ./Core/InfoResultados.py ./Core/Trabajador.py ./Interfaz/RepertorioGdE.py ./BotSearcherIPv6.py ./Core/SC12KModulo.py \
-./Interfaz/RepertorioGdR.py ./Interfaz/RepertorioGdM.py \
---introspect-only --no-sourcecode --graph ALL
